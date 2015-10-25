97f1c7f35c493e2a9a87758a822e3ca7cd98a5a7
pek-github <pkaklam@gmail.com>
2015-10-25 17:48:31 +0200

    function for calculating the minimum divisor. plus tests

--- src/numbers/core.clj
+++ src/numbers/core.clj
@@ -1,5 +1,29 @@
 (ns numbers.core)
 
+(defn min-divisor 
+  "find the minimum divisor of a given number" 
+  [num]
+
+  (cond
+    (= num 0) 0  ; TODO: throw Exception
+    (< num 0) -1 ; TODO: throw Exception
+    (= num 1) 1
+    (even? num) 2
+    :else
+      (loop [k 3]
+        (if (> (* k k) num)
+          num
+          (if (= 0 (mod num k)) 
+            k 
+            (recur (+ k 2))
+          )
+        )
+      )
+  )
+)
+
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
--- test/numbers/core_test.clj
+++ test/numbers/core_test.clj
@@ -2,6 +2,38 @@
   (:require [clojure.test :refer :all]
             [numbers.core :refer :all]))
 
+(deftest minimum-divisor-test
+  (testing "Prime number 2"
+    (is (= 2 (min-divisor 2))))
+
+  (testing "Prime number 3"
+    (is (= 3 (min-divisor 3))))
+
+  (testing "Prime number 17"
+    (is (= 17 (min-divisor 17))))
+
+  (testing "Non-Prime number 6"
+    (is (= 2 (min-divisor 6))))
+
+  (testing "Prime number 35"
+    (is (= 5 (min-divisor 35))))
+
+  (testing "Edge case: number 1"
+    (is (= 1 (min-divisor 1))))
+
+  (testing "Edge case: number 0"
+    (is (= 0 (min-divisor 0))))
+
+  (testing "Edge case: negative integer: -11"
+    (is (= -1 (min-divisor -11))))
+)
+
+
+
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 0)))
+
+  (testing "FIXME, I fail. No.2"
+    (is (= 1 1)))
+)
